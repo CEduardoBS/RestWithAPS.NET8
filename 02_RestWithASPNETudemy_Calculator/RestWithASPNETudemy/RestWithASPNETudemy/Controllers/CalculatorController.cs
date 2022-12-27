b9c199cb72300c1a5651ea1a35cd9b95bf9321e2
unknown <Administrador@GUI00368.lojasguido.local>
2022-12-27 15:27:47 -0300

    My tentative

--- 02_RestWithASPNETudemy_Calculator/RestWithASPNETudemy/RestWithASPNETudemy/Controllers/CalculatorController.cs
+++ 02_RestWithASPNETudemy_Calculator/RestWithASPNETudemy/RestWithASPNETudemy/Controllers/CalculatorController.cs
@@ -15,7 +15,7 @@ namespace RestWithASPNETudemy.Controllers
             _logger = logger;
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        [HttpGet("{operation}/{firstNumber}/{secondNumber}")]
         public IActionResult Get(String operation, String firstNumber, String secondNumber)
         {
             if (operation == "sum") {
@@ -24,7 +24,42 @@ namespace RestWithASPNETudemy.Controllers
                     var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                     return Ok(sum.ToString());
                 }
-            } 
+            } else if (operation == "sub")
+            {
+                if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                {
+                    var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                    return Ok(sub.ToString());
+                }
+            } else if (operation == "multi")
+            {
+                if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                {
+                    var multi = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                    return Ok(multi.ToString());
+                }
+            } else if (operation == "div")
+            {
+                if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                {
+                    var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                    return Ok(div.ToString());
+                }
+            } else if (operation == "raiz")
+            {
+                if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                {
+                    var raiz = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber));
+                    return Ok(raiz.ToString());
+                }
+            } else if (operation == "media")
+            {
+                if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                {
+                    var media = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                    return Ok(media.ToString());
+                }
+            }
 
             return BadRequest("Invalid Input");
         }
